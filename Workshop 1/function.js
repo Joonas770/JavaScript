2405b89e79ea2e7f1d494ca2b4f78f9394e0a1c8
Joonas770 <jos00117@laurea.fi>
2025-09-05 04:39:10 +0300

    Workshop 1

---
+++ Workshop 1/function.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+return a + b;
+
+}
+var sum = addNumbers(5, 100)
+if (sum > 100) { 
+    alert("Sum of numbers are over 100")
+}
+document.write(sum)
---
+++ Workshop 1/loop.js
@@ -0,0 +1,4 @@
+for (i = 1; i<15; i++) {
+var number = (Math.floor(Math.random() * 100) + 1);
+document.write(number + "\n" )
+}
